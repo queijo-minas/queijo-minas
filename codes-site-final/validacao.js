7e84433c0590b46495ff441e2c979e19c74b14dd
anavitxria <ana.gomes@sptech.school>
2024-11-07 15:06:20 -0200

    subindo dash lincada ao site

---
+++ codes-site-final/validacao.js
@@ -0,0 +1,147 @@
+function validarSenha(){
+    const senha = senha_input.value 
+
+    if( senha.length >= 7 ){
+        validacaoSenha.innerHTML = `<div style="color: green;">Senha Válida</div><br>`
+    }else {
+        validacaoSenha.innerHTML = `<div style="color: red;">Sua senha está fraca</div><br>`
+    }
+
+
+}
+
+function confirmacaoSenha(){
+    const senha = senha_input.value
+    const confirmacaoSenha = confirmacao_senha_input.value
+    
+    if(senha == confirmacaoSenha){
+        validacaoConfirmacao.innerHTML = `<div style="color: green;">Senha Válida</div><br>`
+    }else{
+        validacaoConfirmacao.innerHTML = `<div style="color: red;">As senhas devem ser a mesma</div><br>`
+    }
+}
+
+function gerarSenha(){
+var senhaAutoGerada = ``
+var Catalogo = `aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPrRsStTuUvVwWxXyYzZ0123456789!@#$%&*_-+=`
+
+
+for(i=0; i<7; i++){
+    senhaAutoGerada += Catalogo[parseInt(Math.random() * 71)]
+}
+
+senha_input.value = senhaAutoGerada
+senha_input.type = "text"
+confirmacao_senha_input.value = senhaAutoGerada
+confirmacao_senha_input.type = "text"
+
+validarSenha()
+confirmacaoSenha()
+
+}
+
+
+
+function Validacao() {
+
+        alert("Bem Vindo a Queijo no Ponto, obrigado pela confiança!")
+}
+
+// Estrutura do CNPJ 99.999.999/9999-99
+
+function validarCNPJ(){
+const CNPJ = cnpj_input.value
+var fraseCorrigida = ``
+
+//if( colocar condição de validar o campo se tem os '.', '/', '-' )
+    if( CNPJ.length == 14){
+    for ( i = 0 ; i < CNPJ.length ; i++){
+
+        
+        if(i == 2 || i == 5){
+            fraseCorrigida += `.`
+        }
+        if (i == 8){
+            fraseCorrigida += `/`
+        }
+        if (i == 12){
+            fraseCorrigida += `-`
+        }
+        
+        fraseCorrigida += CNPJ[i]
+        cnpj_input.value = fraseCorrigida
+        validacaoCNPJ.innerHTML =
+        `<div style="color: green;">CNPJ válido, ${fraseCorrigida}</div><br>`
+
+    }
+    } else {
+        validacaoCNPJ.innerHTML = `<div style="color: red;">CNPJ inválido, insira apenas os números necessários (14 digitos)</div><br>`
+    }
+
+
+}
+
+
+function validarEmail(){
+    const email = email_input.value 
+
+    if( email.indexOf('@') > -1 && email.indexOf('.com') > -1){
+        validacaoEmail.innerHTML = `<div style="color: green;">E-mail Válido</div><br>`
+    }else {
+        validacaoEmail.innerHTML = `<div style="color: red;">Sua e-mail está inválido</div><br>`
+    }
+
+}
+
+
+function validarCPF(){
+    const cpf = cpf_input.value 
+    var cpfSemantico = ``
+
+    if(cpf.length == 11){
+        for( posição = 0; posição < 11; posição ++) {
+            if(posição == 3 || posição == 6){
+                cpfSemantico += `.`
+            }if (posição == 9){
+                cpfSemantico += `-`
+            }
+
+            cpfSemantico += cpf[posição]
+
+            validacaoCPF.innerHTML = `<div style="color: green;">Seu CPF está válido ${cpfSemantico}</div><br>`
+
+            cpf_input.value = cpfSemantico
+
+        }
+    }else {
+        validacaoCPF.innerHTML = `<div style="color: red;">CPF inválido, insira apenas a quantidade correta de números (11)</div><br>`
+    }
+
+
+}
+
+function validarTelefone(){
+    const telefone = telefone_input.value 
+    var telefoneSemantico = ``
+
+    if(telefone.length == 11){
+        for( posição = 0; posição < 11; posição ++) {
+            if(posição == 3 || posição == 6){
+                cpfSemantico += `.`
+            }if (posição == 9){
+                cpfSemantico += `-`
+            }
+
+            cpfSemantico += cpf[posição]
+
+            validacaoCPF.innerHTML = `<div style="color: green;">Seu CPF está válido ${cpfSemantico}</div><br>`
+
+            telefone_input.value = cpfSemantico
+
+        }
+    }else {
+        validacaoCPF.innerHTML = `<div style="color: red;">CPF inválido, insira apenas a quantidade correta de números (11)</div><br>`
+    }
+
+
+}
