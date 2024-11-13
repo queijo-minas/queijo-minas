3d20bb206b240457454aa91c4581c14cb1ef0d0b
Pedro-Lion <pedro.santos033@sptech.school>
2024-11-13 19:43:14 -0300

    update validações

--- Codes/codes-site-final/Public/script/validacao.js
+++ Codes/codes-site-final/Public/script/validacao.js
@@ -198,44 +198,95 @@ function confirmacaoSenha() {
 
 function gerarSenha() {
     var senhaAutoGerada = ``;
-    var Catalogo = `aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPrRsStTuUvVwWxXyYzZ0123456789!@#$%&*_-+=`;
+    var Catalogo = `aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPrRsStTuUvVwWxXyYzZ0123456789`
+    var caractereEspecial = ``
 
-    for (i = 0; i < 7; i++) {
-        senhaAutoGerada += Catalogo[parseInt(Math.random() * 71)];
+    for (i = 0; i < 6; i++) {
+        senhaAutoGerada += Catalogo[parseInt(Math.random() * 60)];
     }
 
-    senha_input.value = senhaAutoGerada;
+    for(i = 0; i < 1; i++){
+        senhaAutoGerada += Catalogo[parseInt(Math.random() * 11)];
+    }
+    var novaSenha = ``
+
+    for(posicao = 7; posicao = 0; posicao--){
+        var caractere = ``
+        caractere = senhaAutoGerada[parseInt(Math.random()*posicao)]
+        senhaAutoGerada = senhaAutoGerada.replaceAll(caractere, "")
+        novaSenha += caractere
+
+    }
+
+    senha_input.value = novaSenha;
     senha_input.type = "text";
-    confirmacao_senha_input.value = senhaAutoGerada;
+    confirmacao_senha_input.value = novaSenha;
     confirmacao_senha_input.type = "text";
 
     validarSenha();
     confirmacaoSenha();
 }
 
-
+function tirarPontuacao(texto=''){
+    var textoFinal = ``
+    for(posicao=0;posicao < texto.length;posicao++){
+        if(texto[posicao]==Number(texto[posicao])){
+            textoFinal += texto[posicao]
+        }
+    }
+    return textoFinal
+}
 function validarCPF() {
-    const cpf = cpf_input.value;
+    var cpf = cpf_input.value;
     var cpfSemantico = ``;
+    cpf = tirarPontuacao(cpf)
+
+    for (var posição = 0; posição < cpf.length; posição++) {
+        if (posição == 3 || posição == 6) cpfSemantico += `.`;
+        if (posição == 9) cpfSemantico += `-`;
+        cpfSemantico += cpf[posição];
+    }
 
     if (cpf.length == 11) {
-        for (let posição = 0; posição < 11; posição++) {
-            if (posição == 3 || posição == 6) cpfSemantico += `.`;
-            if (posição == 9) cpfSemantico += `-`;
-            cpfSemantico += cpf[posição];
-        }
-
         validacaoCPF.innerHTML = `<div style="color: green;">Seu CPF está válido ${cpfSemantico}</div><br>`;
         cpf_input.value = cpfSemantico;
+        var cpfFinal = cpf
     } else {
         validacaoCPF.innerHTML = `<div style="color: red;">CPF inválido, insira apenas a quantidade correta de números (11)</div><br>`;
+        cpf_input.value = cpfSemantico;
+        cpfFinal = null
+    }
+}
+
+// Estrutura do CNPJ 99.999.999/9999-99
+function validarCNPJ() {
+    var cnpj = cnpj_input.value;
+    var cnpjSemantico = ``;
+    cnpj = tirarPontuacao(cnpj)
+   
+    for (var posição = 0; posição < cnpj.length; posição++) {
+        if (posição == 2 || posição == 5) cnpjSemantico += `.`;
+        if (posição == 8) cnpjSemantico += `/`;
+        if (posição == 12) cnpjSemantico += `-`;
+        cnpjSemantico += cnpj[posição];
+    }
+
+    if (cnpj.length == 14) {
+      
+
+        validacaoCNPJ.innerHTML = `<div style="color: green;">Seu CNPJ está válido ${cnpjSemantico}</div><br>`;
+        cnpj_input.value = cnpjSemantico;
+    } else {
+        validacaoCNPJ.innerHTML = `<div style="color: red;">CNPJ inválido, insira apenas a quantidade correta de números (14)</div><br>`;
+        cnpj_input.value = cnpjSemantico;
+
     }
 }
 
 // o trim tira os espaços em branco
 function cadastrarUsuario() {
     const nomeVar = document.querySelector(".signup-form .input-box input[placeholder='Digite seu nome']").value.trim();
-    const cpfVar = cpf_input.value.trim();
+    const cpfVar = cpfFinal.trim();
     const emailVar = email_input.value.trim();
     const telefoneVar = telefone_input.value.trim();
     const senhaVar = senha_input.value.trim();
