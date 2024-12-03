6eaa3b73ebfa920e211a2f6e0522925a10124467
sahdamacena <samara.damacena@sptech.school>
2024-12-03 18:22:30 -0300

    Subindo rota da KPI

--- Codes/codes-site-final/src/controllers/medidaController.js
+++ Codes/codes-site-final/src/controllers/medidaController.js
@@ -24,11 +24,11 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+    var idDadosSensor = req.params.idDadosSensor;
 
     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idDadosSensor).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
