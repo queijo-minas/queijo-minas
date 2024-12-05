1d93d6df6ead4f230531937d22f5a298fc94c26f
Diaseduarda01 <maria.dias@sptech.school>
2024-12-05 13:44:39 -0300

    mudança de ambiente para VM

--- Codes/codes-site-final/app.js
+++ Codes/codes-site-final/app.js
@@ -1,5 +1,6 @@
 // Definindo ambiente e caminho do arquivo .env
-var ambiente_processo = 'desenvolvimento'; 
+// var ambiente_processo = 'desenvolvimento'; 
+var ambiente_processo = 'producao';
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 
 require("dotenv").config({ path: caminho_env });
